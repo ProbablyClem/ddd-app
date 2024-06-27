f9eebf3067372d1332166fb83a038f0179c5d43f
Clément Guiton <clement.guiton.dev@gmail.com>
2024-06-27 16:58:45 +0200

    parser

--- src/data/models.rs
+++ src/data/models.rs
@@ -2,64 +2,64 @@ use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 pub struct RawOrder {
-    order_id: String,
-    customer_id: String,
-    order_status: String,
-    order_purchase_timestamp: String,
-    order_approved_at: String,
-    order_delivered_carrier_date: String,
-    order_delivered_customer_date: String,
-    order_estimated_delivery_date: String,
+    pub order_id: String,
+    pub customer_id: String,
+    pub order_status: String,
+    pub order_purchase_timestamp: String,
+    pub order_approved_at: String,
+    pub order_delivered_carrier_date: String,
+    pub order_delivered_customer_date: String,
+    pub order_estimated_delivery_date: String,
 }
 
 #[derive(Debug, Deserialize)]
 pub struct RawOrderItem {
-    order_id: String,
-    order_item_id: String,
-    product_id: String,
-    seller_id: String,
-    shipping_limit_date: String,
-    price: f64,
-    freight_value: f64,
+    pub order_id: String,
+    pub order_item_id: String,
+    pub product_id: String,
+    pub seller_id: String,
+    pub shipping_limit_date: String,
+    pub price: f64,
+    pub freight_value: f64,
 }
 
 #[derive(Debug, Deserialize)]
 pub struct RawProduct {
-    product_id: Option<String>,
-    product_category_name: Option<String>,
-    product_name_lenght: Option<i64>,
-    product_description_lenght: Option<i64>,
-    product_photos_qty: Option<i64>,
-    product_weight_g: Option<f64>,
-    product_length_cm: Option<f64>,
-    product_height_cm: Option<f64>,
-    product_width_cm: Option<f64>,
+    pub product_id: Option<String>,
+    pub product_category_name: Option<String>,
+    pub product_name_lenght: Option<i64>,
+    pub product_description_lenght: Option<i64>,
+    pub product_photos_qty: Option<i64>,
+    pub product_weight_g: Option<f64>,
+    pub product_length_cm: Option<f64>,
+    pub product_height_cm: Option<f64>,
+    pub product_width_cm: Option<f64>,
 }
 
 #[derive(Debug, Deserialize)]
 pub struct RawCustomer {
-    customer_id: String,
-    customer_unique_id: String,
-    customer_zip_code_prefix: String,
-    customer_city: String,
-    customer_state: String,
+    pub customer_id: String,
+    pub customer_unique_id: String,
+    pub customer_zip_code_prefix: String,
+    pub customer_city: String,
+    pub customer_state: String,
 }
 
 #[derive(Debug, Deserialize)]
 pub struct RawSeller {
-    seller_id: String,
-    seller_zip_code_prefix: String,
-    seller_city: String,
-    seller_state: String,
+    pub seller_id: String,
+    pub seller_zip_code_prefix: String,
+    pub seller_city: String,
+    pub seller_state: String,
 }
 
 #[derive(Debug, Deserialize)]
 pub struct RawReview {
-    review_id: String,
-    order_id: String,
-    review_score: i64,
-    review_comment_title: String,
-    review_comment_message: String,
-    review_creation_date: String,
-    review_answer_timestamp: String,
+    pub review_id: String,
+    pub order_id: String,
+    pub review_score: i64,
+    pub review_comment_title: String,
+    pub review_comment_message: String,
+    pub review_creation_date: String,
+    pub review_answer_timestamp: String,
 }
--- src/domain/commerce.rs
+++ src/domain/commerce.rs
@@ -6,3 +6,48 @@ pub struct CommerceDashboardData {}
 pub async fn get_commerce_dashboard() -> Json<CommerceDashboardData> {
     Json(CommerceDashboardData {})
 }
+
+/// 10 best rated products
+fn best_rated_products() -> Vec<String> {
+    let raw_reviews = crate::data::loader::load_reviews();
+    let raw_products = crate::data::loader::load_products();
+    let mut product_reviews = std::collections::HashMap::new();
+    for review in raw_reviews {
+        let product_id = review.order_id;
+        let score = review.review_score;
+        let count = product_reviews.entry(product_id).or_insert((0, 0));
+        count.0 += score;
+        count.1 += 1;
+    }
+    let mut product_scores = Vec::new();
+    for (product_id, (total_score, count)) in product_reviews {
+        let average_score = total_score as f64 / count as f64;
+        product_scores.push((product_id, average_score));
+    }
+    product_scores.sort_by(|a, b| b.1.partial_cmp(&a.1).unwrap());
+    let best_rated_products = product_scores
+        .iter()
+        .take(10)
+        .map(|(product_id, _)| {
+            raw_products
+                .iter()
+                .find(|product| product.product_id == Some(product_id.clone()))
+                .unwrap()
+        })
+        .collect::<Vec<_>>();
+    best_rated_products
+        .iter()
+        .map(|product| product.product_category_name.clone().unwrap())
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_best_rated_products() {
+        let best_rated_products = best_rated_products();
+        assert_eq!(best_rated_products.len(), 10);
+    }
+}
